6879d4bb1cb74183b99e8d92a29dc36d9a8a7bf7
q87718350 <77486085+q87718350@users.noreply.github.com>
2023-12-27 06:34:15 +0000

    Merge branch 'fixing-unable-get-bgimage-and-favico'
    # Conflicts:
    #	docker-compose.yml
    #	k8s/ingress.yaml

--- frontend/src/app/Home/Home.component.ts
+++ frontend/src/app/Home/Home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button'
 @Component({
   selector: 'app-Home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './Home.component.html',
   styleUrl: './Home.component.scss'
 })
 export class Home {
-
+  constructor() {
+    console.log("happy");
+  }
 }
