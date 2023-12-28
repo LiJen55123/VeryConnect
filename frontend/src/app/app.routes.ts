cf293aaf31438cced1d4492e16be5ea30e9a2a83
q87718350 <77486085+q87718350@users.noreply.github.com>
2023-12-28 11:19:48 +0000

    welcome page ready for adding icon

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import {Routes} from '@angular/router';
 import {Home} from './Home/Home.component';
 import {AboutComponent} from './about/about.component'
+import { WelcomeComponent } from './welcome/welcome.component'
 export const routes: Routes = [
+  {
+    path: '',
+    title: 'App about Page',
+    component: WelcomeComponent,
+  },
   {
     path: 'home',
     title: 'App Home Page',
